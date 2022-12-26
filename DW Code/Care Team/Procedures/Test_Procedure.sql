63326d52337197a4749ea7abac73e06968d17c8d
Rajgithubtrial <rajashekar.kalyanam@capgemini.com>
2022-12-26 17:06:26 +0530

    change in yml with directory

--- DW Code/Care Team/Procedures/Test_Procedure.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE PROCEDURE Test_Prc(name varchar, address varchar)
-RETURNS VARCHAR
-LANGUAGE JAVASCRIPT
-AS
-$$
-var command = "INSERT INTO Employee (emp_id, emp_name, emp_address) VALUES (emp_id.nextval, '"+NAME+"','"+ADDRESS+"')";
-return 'var';
-$$;
